f12146a76243a2915d1368b45dbdb5abb4f5ce53
Fatemeh <fmmajd452@gmail.com>
2019-11-03 17:33:00 +0330

    added categories when saving posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,8 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\DB;
 use App\Http\Resources\Post as PostResource;
 use App\Http\Resources\PostCollection;
 
@@ -17,13 +16,26 @@ class PostController extends Controller
             'title' => 'required|string|min:3|unique:posts',
             'content' => 'required|string|min:255',
             'publish_date' => 'required|date_format:Y-m-d H:i:s',
+            'categories' => 'required|array',
+            'categories.*' => 'string|min:3|max:255',
         ];
 
         $this->validate($request, $rules);
 
+        DB::beginTransaction();
+
         $post = new Post();
         $post->fill($request->only(['title', 'content', 'publish_date']));
         $post->save();
+
+        foreach ($request->input('categories') as $category) {
+            $post->categories()->updateOrCreate(
+                ['name' => $category],
+                ['name' => $category]
+            );
+        }
+
+        DB::commit();
 
         return new PostResource($post);
     }
